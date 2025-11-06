f521bcf8947d49330cb18c1ef0105c9c5808b87c
941-life <truzww@gmail.com>
2025-11-06 21:17:10 +0900

    [feat] connect mySQL

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const app = express();
+const { sequelize } = require('./models');
+const reviewsRouter = require('./routes/reviews');
 
+const app = express();
 app.use(express.json());
 
-const reviewsRouter = require('./routes/reviews');
+sequelize.sync({force:false})
+  .then(()=>{
+    console.log('database 연결 성공');
+  })
+  .catch((err) => {
+    console.error('db 연결 실패', err);
+  });
+
 app.use('/api/reviews', reviewsRouter);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
--- models/review.js
+++ models/review.js
@@ -1,47 +1,22 @@
-let reviews = [];
-let nextId = 1;
+module.exports = (sequelize, DataTypes) => {
+  const Review = sequelize.define('Review', {
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'reviews',
+    timestamps: true,
+    underscored: true
+  });
 
-class ReviewModel {
-  // 전체 조회
-  findAll() {
-    return reviews;
-  }
-  // ID로 조회
-  findById(id) {
-    return reviews.find(r => r.id === id);
-  }
-  // 생성
-  create(data) {
-    const review = {
-      id: nextId++,
-      ...data,
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString()
-    };
-    reviews.push(review);
-    return review;
-  }
-  // 수정
-  update(id, data) {
-    const index = reviews.findIndex(r => r.id === id);
-    if (index === -1) return null;
-
-    reviews[index] = {
-      ...reviews[index],
-      ...data,
-      updatedAt: new Date().toISOString()
-    };
-    return reviews[index];
-  }
-  // 삭제
-  delete(id) {
-    const index = reviews.findIndex(r => r.id === id);
-    if (index === -1) return false;
-
-    reviews.splice(index, 1);
-    return true;
-  }
-}
-
-module.exports = new ReviewModel();
-
+  return Review;
+};
